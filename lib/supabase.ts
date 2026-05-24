245551b83950c0df1757943e9a38639354d964e1
Alexanderjms <alexqnder0318@gmail.com>
2026-05-24 14:03:40 -0600

    Update README and .gitignore; refactor Supabase client to use environment variables

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://fcovxrdfvxjnrmjjvjvn.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjb3Z4cmRmdnhqbnJtamp2anZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2MzE1OTgsImV4cCI6MjA5NTIwNzU5OH0.cvTVl7ET_jhGzJcjJPZyjmzEz00TtUXLh7xQR6R-z3k";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(supabaseUrl, supabaseKey);
